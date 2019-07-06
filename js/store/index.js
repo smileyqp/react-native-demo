edd1196b26d3b09ad08bc6a68b11b65df5cdca68
smileyqp <468417415@qq.com>
2019-07-06 12:00:43 +0800

    add redux middleware logger to simplify debug

--- js/store/index.js
+++ js/store/index.js
@@ -8,9 +8,27 @@ import {middleware} from '../navigator/AppNavigator';
  * @param store
  */
 
+/**
+ * 打印出func的自定义中间件
+ * @param store
+ * @returns {function(*): Function}
+ */
+const logger = store => next => action => {
+    if (typeof action === 'function') {
+        console.log('dispatching a function');
+    } else {
+        console.log('dispatching ', action);
+    }
+    const result = next(action);
+    console.log('nextState ', store.getState());
+    return result;
+};
+
+
 
 const middlewares = [
-    middleware
+    middleware,
+    logger
     
 ];
 
